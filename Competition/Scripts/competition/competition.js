d672d193711561fdd8760f69b10931393ca8db32
Pierre Henry <pierre.henry@salsaaddict.com>
2018-06-12 23:16:54 +0100

    Daily

--- Competition/Scripts/competition/competition.js
+++ Competition/Scripts/competition/competition.js
@@ -4,15 +4,16 @@ var Competition;
 (function (Competition) {
     "use strict";
     var Controller = /** @class */ (function () {
-        function Controller($filter) {
+        function Controller($filter, $log) {
             this.$filter = $filter;
+            this.$log = $log;
             this.judges = ["Alex", "Biskit", "Pierre"]; //, "Dan", "Julian"];
             this.competitors = [
-                { id: 2, name: "Ylenio", results: [] },
-                { id: 1, name: "Sergio", results: [] },
-                { id: 4, name: "Fenix", results: [] },
-                { id: 5, name: "Raj", results: [] },
-                { id: 3, name: "Vaibhav", results: [] }
+                { id: 2, name: "Ylenio", results: [1, 1, 2] },
+                { id: 1, name: "Sergio", results: [2, 3, 1] },
+                { id: 4, name: "Fenix", results: [3, 2, 3] },
+                { id: 5, name: "Raj", results: [4, 4, 5] },
+                { id: 3, name: "Vaibhav", results: [5, 5, 4] }
             ];
         }
         Object.defineProperty(Controller.prototype, "competitorsById", {
@@ -74,28 +75,74 @@ var Competition;
             enumerable: true,
             configurable: true
         });
+        Controller.prototype.majorityCount = function (competitor, rank) {
+            var count = 0;
+            for (var i = 0; i < competitor.results.length; i++) {
+                if (competitor.results[i] <= rank)
+                    count++;
+            }
+            return count;
+        };
         Controller.prototype.simpleMajority = function (rank) {
             var _this = this;
             var output = [];
             this.competitors.forEach(function (competitor) {
-                var count = 0;
-                for (var i = 0; i < competitor.results.length; i++) {
-                    if (competitor.results[i] <= rank) {
-                        count++;
-                        if (count >= _this.majority) {
-                            output.push(competitor);
-                            break;
-                        }
-                    }
-                }
+                if (angular.isDefined(competitor.rank))
+                    return;
+                if (_this.majorityCount(competitor, rank) >= _this.majority)
+                    output.push(competitor);
             });
             return output;
         };
+        Controller.prototype.assignRank = function (rank) {
+            this.$log.debug("assignRank", rank);
+            var simpleMajority = this.simpleMajority(rank);
+            this.$log.debug("simpleMajority", rank, simpleMajority.length, simpleMajority);
+            if (simpleMajority.length === 1) {
+                simpleMajority[0].rank = rank;
+                return;
+            }
+        };
+        Controller.prototype.calculate = function () {
+            this.assignRank(1);
+            this.assignRank(2);
+            this.assignRank(3);
+            this.assignRank(4);
+            this.assignRank(5);
+        };
         Controller.prototype.$postLink = function () { };
-        Controller.$inject = ["$filter"];
+        Controller.$inject = ["$filter", "$log"];
         return Controller;
     }());
     Competition.Controller = Controller;
+    var RankInput;
+    (function (RankInput) {
+        var Controller = /** @class */ (function () {
+            function Controller() {
+            }
+            Controller.prototype.$postLink = function () {
+                var _this = this;
+                this.ngModel.$parsers.unshift(function (value) { return parseInt(value, 10); });
+                this.ngModel.$validators["min"] = function (modelValue, viewValue) { return (modelValue >= 1); };
+                this.ngModel.$validators["max"] = function (modelValue, viewValue) { return (modelValue <= _this.max); };
+            };
+            return Controller;
+        }());
+        RankInput.Controller = Controller;
+        function DirectiveFactory() {
+            var factory = function () {
+                return {
+                    restrict: "A",
+                    controller: Controller,
+                    bindToController: { max: "=rankInput" },
+                    require: { ngModel: "^ngModel" }
+                };
+            };
+            return factory;
+        }
+        RankInput.DirectiveFactory = DirectiveFactory;
+    })(RankInput = Competition.RankInput || (Competition.RankInput = {}));
 })(Competition || (Competition = {}));
 comp.controller("competitionController", Competition.Controller);
+comp.directive("rankInput", Competition.RankInput.DirectiveFactory());
 //# sourceMappingURL=competition.js.map
--- Competition/Scripts/competition/competition.ts
+++ Competition/Scripts/competition/competition.ts
@@ -4,17 +4,17 @@ let comp: angular.IModule = angular.module("Competition", ["ui.bootstrap"]);
 
 namespace Competition {
     "use strict";
-    interface ICompetitor { id: number; name: string; results: number[]; }
+    interface ICompetitor { id: number; name: string; results: number[]; rank?: number; }
     export class Controller implements angular.IController {
-        static $inject: string[] = ["$filter"];
-        constructor(private $filter: angular.IFilterService) { }
+        static $inject: string[] = ["$filter", "$log"];
+        constructor(private $filter: angular.IFilterService, private $log: angular.ILogService) { }
         public judges: string[] = ["Alex", "Biskit", "Pierre"]; //, "Dan", "Julian"];
         public competitors: ICompetitor[] = [
-            { id: 2, name: "Ylenio", results: [] },
-            { id: 1, name: "Sergio", results: [] },
-            { id: 4, name: "Fenix", results: [] },
-            { id: 5, name: "Raj", results: [] },
-            { id: 3, name: "Vaibhav", results: [] }
+            { id: 2, name: "Ylenio", results: [1, 1, 2] },
+            { id: 1, name: "Sergio", results: [2, 3, 1] },
+            { id: 4, name: "Fenix", results: [3, 2, 3] },
+            { id: 5, name: "Raj", results: [4, 4, 5] },
+            { id: 3, name: "Vaibhav", results: [5, 5, 4] }
         ];
         public get competitorsById(): ICompetitor[] { return this.$filter("orderBy")(this.competitors, "id"); }
         public judgeResults(judge: string): number[] {
@@ -63,25 +63,68 @@ namespace Competition {
             }
             return isValid;
         }
+        public doCalc(): void {
+
+        }
+
+
         public get majority(): number { return Math.ceil(this.judges.length / 2); }
+        public majorityCount(competitor: ICompetitor, rank: number): number {
+            let count: number = 0;
+            for (let i: number = 0; i < competitor.results.length; i++) {
+                if (competitor.results[i] <= rank) count++;
+            }
+            return count;
+        }
         public simpleMajority(rank: number): ICompetitor[] {
             let output: ICompetitor[] = [];
             this.competitors.forEach((competitor: ICompetitor): void => {
-                let count: number = 0;
-                for (let i: number = 0; i < competitor.results.length; i++) {
-                    if (competitor.results[i] <= rank) {
-                        count++;
-                        if (count >= this.majority) {
-                            output.push(competitor);
-                            break;
-                        }
-                    }
-                }
+                if (angular.isDefined(competitor.rank)) return;
+                if (this.majorityCount(competitor, rank) >= this.majority) output.push(competitor);
             });
             return output;
         }
+        public assignRank(rank: number): boolean {
+            this.$log.debug("assignRank", rank);
+            let simpleMajority: ICompetitor[] = this.simpleMajority(rank);
+            this.$log.debug("simpleMajority", rank, simpleMajority.length, simpleMajority);
+            if (simpleMajority.length === 1) {
+                simpleMajority[0].rank = rank;
+                return;
+            }
+        }
+        public calculate(): void {
+            this.assignRank(1);
+            this.assignRank(2);
+            this.assignRank(3);
+            this.assignRank(4);
+            this.assignRank(5);
+        }
         public $postLink(): void { }
+    }
+    export namespace RankInput {
+        export class Controller implements angular.IController {
+            public ngModel: angular.INgModelController;
+            public max: number;
+            public $postLink(): void {
+                this.ngModel.$parsers.unshift(function (value: any): number { return parseInt(value, 10); });
+                this.ngModel.$validators["min"] = (modelValue: number, viewValue: string): boolean => { return (modelValue >= 1); }
+                this.ngModel.$validators["max"] = (modelValue: number, viewValue: string): boolean => { return (modelValue <= this.max); }
+            }
+        }
+        export function DirectiveFactory(): angular.IDirectiveFactory {
+            let factory = function (): angular.IDirective {
+                return {
+                    restrict: "A",
+                    controller: Controller,
+                    bindToController: { max: "=rankInput" },
+                    require: { ngModel: "^ngModel" }
+                };
+            };
+            return factory;
+        }
     }
 }
 
 comp.controller("competitionController", Competition.Controller);
+comp.directive("rankInput", Competition.RankInput.DirectiveFactory());
